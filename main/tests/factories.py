33fe56b6382d2bedb96651404c04a88f4b74400b
Kirill Kirsanov <montego@yandex.ru>
2023-11-28 22:27:26 +0300

    Tests for api added

---
+++ main/tests/factories.py
@@ -0,0 +1,12 @@
+import factory
+from factory.django import DjangoModelFactory
+from main.models import Book
+
+class BookFactory(DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    name = factory.Faker('text', max_nb_chars=20)
+    author = factory.Faker('name')
+    year_published = factory.Faker('pyint', min_value=0, max_value=2023)
+    isbn = factory.Faker('isbn13')
---
+++ main/tests/test_api_book.py
@@ -0,0 +1,97 @@
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+import factory
+from main.models import Book
+from main.serializers import BookSerializer
+from factories import BookFactory
+from django.urls import reverse
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+@pytest.fixture
+def book():
+    return BookFactory()
+
+
+@pytest.mark.django_db
+def test_create_book(client):
+    data = factory.build(dict, FACTORY_CLASS=BookFactory)
+    response = client.post(reverse('book-list'), data)
+    assert response.status_code == status.HTTP_201_CREATED
+    assert Book.objects.last() is not None
+
+
+@pytest.mark.django_db
+def test_update_book(client, book):
+    data = factory.build(dict, FACTORY_CLASS=BookFactory)
+    response = client.put(reverse('book-detail', kwargs={'pk': book.id}), data)
+   
+    expected_data = {
+        'id': book.id,
+        'name': data['name'],
+        'author': data['author'],
+        'year_published': data['year_published'],
+        'isbn': data['isbn'],
+    }
+    
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == expected_data
+
+
+@pytest.mark.django_db
+def test_partial_update_book(client, book):
+    data = factory.build(dict, FACTORY_CLASS=BookFactory)
+    data.pop('year_published')
+    data.pop('isbn')
+    response = client.patch(reverse('book-detail', kwargs={'pk': book.id}), data)
+   
+    expected_data = {
+        'id': book.id,
+        'name': data['name'],
+        'author': data['author'],
+        'year_published': book.year_published,
+        'isbn': book.isbn,
+    }
+    
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == expected_data
+
+
+@pytest.mark.django_db
+def test_retrieve_book(client, book):
+    response = client.get(reverse('book-detail', kwargs={'pk': book.id}))
+   
+    expected_data = {
+        'id': book.id,
+        'name': book.name,
+        'author': book.author,
+        'year_published': book.year_published,
+        'isbn': book.isbn,
+    }
+    
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == expected_data
+
+
+@pytest.mark.django_db
+def test_list_book(client):
+    books = BookFactory.create_batch(9)
+    
+    response = client.get(reverse('book-list'))
+   
+    expected_data = BookSerializer(books, many=True).data
+    
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == expected_data
+
+
+@pytest.mark.django_db
+def test_delete_book(client, book):
+    response = client.delete(reverse('book-detail', kwargs={'pk': book.id}))
+   
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert Book.objects.last() is None
